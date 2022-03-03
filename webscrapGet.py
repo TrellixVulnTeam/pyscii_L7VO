d803b8961e5c9f42e6155b0fa00efe9e9b20059a
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-03 18:59:41 +0100

    Modified what's in ifmain

--- webscrapGet.py
+++ webscrapGet.py
@@ -5,14 +5,9 @@ def getASCII(siteURL : str):
     driverOptions = webdriver.FirefoxOptions()
     driverOptions.headless = True
     
-    """try:
-        driverOptions.set_headless(headless=True)
-    except AttributeError:
-        driverOptions.add_argument("--headless")"""
-    
     driver = webdriver.Firefox(options=driverOptions)
-    
     driver.get(siteURL)
+    
     asciiArt = driver.find_element_by_id("taag_output_text")
 
     if not asciiArt:
@@ -21,5 +16,6 @@ def getASCII(siteURL : str):
     return asciiArt.text
 
 
-a = getASCII("https://patorjk.com/software/taag/#p=display&f=Big&t=Type%20Something")
-print(a)
+if __name__ == "__main__":
+    asciiArt = getASCII("https://patorjk.com/software/taag/#p=display&f=Big&t=PyScii")
+    print(asciiArt)
